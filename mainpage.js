7e623d872cead9ee43ce7a783c7c7a3672286fd6
Datngo2001 <ngominhdat110115@gmail.com>
2020-09-04 21:47:09 +0700

    add login portal

--- mainpage.js
+++ mainpage.js
@@ -2,3 +2,7 @@ function DropDown(){
     var rightNavBav = document.getElementById('right-navBav');
     rightNavBav.classList.toggle('drop');
 }
+function onOFFLogin(event){
+    document.getElementById('loginPortal-container').classList.toggle('show');
+    console.log(event);
+}
