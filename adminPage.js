7ccb8043bf5905d1ccb11e7ae9e5aa637e956641
Datngo2001 <ngominhdat110115@gmail.com>
2020-09-05 15:58:34 +0700

    add admin page with navbar, login

---
+++ adminPage.js
@@ -0,0 +1,9 @@
+function DropDown(){
+    var rightNavBav = document.getElementById('right-navBav');
+    rightNavBav.classList.toggle('drop');
+}
+function onOFFLogin(){
+    document.getElementById('loginPortal-container').classList.toggle('show');
+}
+function viewPostPanel(){
+}
