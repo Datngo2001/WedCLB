41d6b2e901c899689d72636321f680279faa6ae1
Ngô Đạt <supodatngo123@gmail.com>
2021-08-12 09:23:11 +0700

    final commit

--- resources/js/mainpage.js
+++ resources/js/mainpage.js
@@ -1,19 +1,20 @@
-function DropDown(){
+function DropDown() {
     var rightNavBav = document.getElementById('right-navBav');
     rightNavBav.classList.toggle('drop');
 }
-function onOFFLogin(){
+function onOFFLogin() {
     document.getElementById('loginPortal-container').classList.toggle('show');
 }
-var navbar = document.getElementById('navBar');
-var navBarPlaceHolder = document.getElementById('navBarPlaceHolder');
-var navBarPosition_old = navBarPlaceHolder.getBoundingClientRect().top;
-function activeNavBarWhenScrollUp(){
+
+var navBarPosition_old;
+function activeNavBarWhenScrollUp() {
+    var navbar = document.getElementById('navBar');
+    var navBarPlaceHolder = document.getElementById('navBarPlaceHolder');
     var navBarPosition_new = navBarPlaceHolder.getBoundingClientRect().top;
     //navBarPosition_new and navBarPosition_old are negative
-    if (navBarPosition_new > navBarPosition_old){
+    if (navBarPosition_new > navBarPosition_old) {
         navbar.classList.remove('scrollDownDisappear');
-    }else{
+    } else {
         navbar.classList.add('scrollDownDisappear');
     }
     navBarPosition_old = navBarPosition_new;
